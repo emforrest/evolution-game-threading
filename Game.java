0efa12be8f82eb1e580ea5e45941a535c5e656a7
emforrest <69385208+emforrest@users.noreply.github.com>
2024-01-23 11:37:05 +0000

    made a start

---
+++ Game.java
@@ -0,0 +1,22 @@
+/**
+ * A simulation of the Evolution game using threading
+ * @author Eleanor Forrest
+ */
+
+public class Game {
+    /**
+     * The executable main function to initialise and start the game
+     * @param args - none
+     */
+    public static void main(String[] args){
+
+    }
+
+    /**
+     * Ask the user to enter the number of children 
+     * @return numChildren - the number of children playing the game. Between ######################
+     */
+    private static void getNumberOfChildren(){
+
+    }
+}
